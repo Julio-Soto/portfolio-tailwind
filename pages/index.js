27f2d9ae54fdbbb9af4785862389d6e939d506ec
Julio Soto <juliosotodev@gmail.com>
2022-10-16 23:10:26 -0600

    finished tools section

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,12 @@ import styles from '../styles/Home.module.css'
 import {BsFillMoonStarsFill} from 'react-icons/bs'
 import {AiFillTwitterCircle,AiFillLinkedin,AiFillYoutube} from 'react-icons/ai'
 import deved from '../public/dev-ed-wave.png'
+import design from '../public/design.png'
+import code from '../public/code.png'
+import consulting from '../public/consulting.png'
+
+
+
 export default function Home() {
   return (
     <div>
@@ -40,7 +46,25 @@ export default function Home() {
         </section>
         <section>
           <h3 className='text-3xl py-1'>Services I offer</h3>
-          <p className='text-md py-2 leading-8 text-gray-800'></p>
+          <p className='text-md py-2 leading-8 text-gray-800'>
+            Since the beginning of my journey as a freelance designer and developer, I've done remote work for 
+            <span className='text-teal-500'> agencies </span>
+            consulted for <span className='text-teal-500'> startups </span>
+            and collaborated with talented people to create digital products for
+            both bussiness and consumer use.
+          </p>
+          <p className='text-md py-2 leading-8 text-gray-800'>
+            I offer from a wide range of services, including brand design, programming, and teaching.
+          </p>
+          <div>
+            <Image src={design} alt='' width={100} height={100} />
+            <h3 className='text-lg font-medium pb-2 pt-8'>Beautiful Designs</h3>
+            <p className='py-2'>Creating elegant designs suited for your needs following core design theory.</p>
+            <h4 className='py-4 text-teal-600'>Desing tools I use.</h4>
+            <p className='text-gray-800 py-1'>Photoshop</p>
+            <p className='text-gray-800 py-1'>Illustrator</p>
+            <p className='text-gray-800 py-1'>Figma</p>
+          </div>
         </section>
       </main>
 
